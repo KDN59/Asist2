23a3384434ad6b0872b423dd051f551f851614fd
KDN59 <kdn59@mail.ru>
2019-08-26 16:11:01 +0300

    Initial Commit

---
+++ KalkanAsist2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  KalkanAsist2
+//
+//  Created by KDN59 on 26.08.2019.
+//  Copyright © 2019 KDN59. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KalkanAsist2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  KalkanAsist2
+//
+//  Created by KDN59 on 26.08.2019.
+//  Copyright © 2019 KDN59. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
